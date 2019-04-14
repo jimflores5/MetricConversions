61c28ec60516f690ba17ccbd5af7975b7358ca14
Jim Flores <jimflores5@gmail.com>
2019-04-14 17:48:21 -0500

    Added answer checking to metric.py.  Form responses can now be evaluated and feedback given.

--- metrics.py
+++ metrics.py
@@ -13,14 +13,14 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 app.secret_key = 'yrtsimehc'
 
-def checkAnswer(units, answer, value):
+def checkAnswer(correct_answer, answer):
     if answer == '':        #Check for null result.
         return False
 
     if answer[0] == ".":    #Convert '.xx' to '0.xx'.
         answer = "0"+answer
 
-    if answer == convertValue(units, value):    #Check for exact result.
+    if answer == correct_answer:    #Check for exact result.
         return True
     else:
         return False
@@ -30,30 +30,36 @@ def index():
     #session.clear()
     return render_template('index.html',title="Metric Practice")
 
-@app.route('/conversion_practice/<prefixes>', methods=['POST', 'GET'])
-def conversion_practice(prefixes):
+@app.route('/conversion_practice/<type>', methods=['POST', 'GET'])
+def conversion_practice(type):
     practiceList = []
     answers = []
     if request.method == 'POST':
-        answer = request.form['answer']
-        units = request.form['units']
-        value = request.form['value']
-        if checkAnswer(units, answer, value):
-            flash('Correct!  :-)', 'correct')
-        else:
-            flash('Try again, or click here to reveal the answer.', 'error')
+        numCorrect = 0
+        for item in range(4):
+            answers.append(request.form['answer'+str(item)])  #Pull user answers into a list.
+            given_units = request.form['given_units'+str(item)]
+            desired_units = request.form['desired_units'+str(item)]
+            orig_value = request.form['value'+str(item)]
+            correct_answer = request.form['correct_answer'+str(item)]
+            practiceList.append([orig_value, correct_answer, given_units, desired_units]) #Pull original data into a list.
+            if checkAnswer(correct_answer, answers[item]):
+                numCorrect += 1
+                flash('Correct!  :-)', 'correct')
+            else:
+                flash('Try again, or click here to reveal the answer.', 'error')
         
-        return render_template('countingSigFigs.html', value=value, units = units, answer = answer, prefixes = prefixes)
+        return render_template('conversionPractice.html', practiceList = practiceList, answers = answers, type = type, checked = True, numCorrect = numCorrect)
 
     while len(practiceList) <= 4:
-        units = selectUnits(prefixes)              #Generate starting & ending units
+        units = selectUnits(type)              #Generate starting & ending units
         sigFigs = random.randrange(1,4)
         power = random.randrange(-3,4)
         value = Number(sigFigs, power, units)
-        if value not in practiceList:
+        if value not in practiceList:       #TODO: Make practiceList format consistent between GET and POST events.
             practiceList.append(value)
 
-    return render_template('conversionPractice.html',title="Metric Conversion Practice", practiceList = practiceList, answers = answers, prefixes = prefixes)
+    return render_template('conversionPractice.html',title="Metric Conversion Practice", practiceList = practiceList, answers = answers, type = type, checked = False)
 
 if __name__ == '__main__':
     app.run()
